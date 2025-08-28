71c805a4c411cf67dd054928ed5bf6f428ee942e
igor <igorek888rane@gmail.com>
2025-08-28 12:35:40 +0300

    resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import Kit from "./assets/kit.jpg";
-
+import { pdfjs } from "react-pdf";
+import { Document, Page } from "react-pdf";
 import "./App.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
 
 function App() {
   return (
-    <>
-      <img src={Kit} className="logo react" alt="React logo" />
-    </>
+    <Document file="resume.pdf" noData={null} loading={null} error={null}>
+      <Page
+        scale={1.05}
+        pageNumber={1}
+        renderAnnotationLayer={false}
+        renderTextLayer={false}
+      />
+    </Document>
   );
 }
 
